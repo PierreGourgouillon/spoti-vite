1e95b70feee248eea21af0b04cb27c365eea378a
Pierre <Pierre_gourgouillon@hotmail.com>
2023-06-16 11:36:03 +0200

    Create .env file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,6 @@
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="font-black">{ import.meta.env.VITE_CLIENT_ID }</h1>
 }
 
 export default App
